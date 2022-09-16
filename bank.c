233e3a09ea9b331ebaa7976de9b733e1ee15bf29
Mahi Maliha <87617184+MahiMaliha@users.noreply.github.com>
2022-09-16 19:53:28 +0600

    added main function

--- bank.c
+++ bank.c
@@ -23,3 +23,58 @@ void divider()
         printf("-");
     }
 }
+int main()
+{
+    FILE *ptr = fopen("Account.txt", "w");
+
+    int ch;
+    printf("Enter your name : \n");
+    gets(name);
+    fprintf(ptr, "\nName : %s\n", name);
+    printf("Enter your account no. : ");
+    scanf("%d", &acc_no);
+    fprintf(ptr, "Account no. : %d\n", acc_no);
+
+    fclose(ptr);
+    while (1)
+    {
+        menu();
+        printf("\nEnter your choice :\n");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            system("cls");
+            deposit_money();
+            break;
+        case 2:
+            system("cls");
+            withdraw_money();
+            break;
+
+        case 3:
+            system("cls");
+            transfer_money();
+            break;
+
+        case 4:
+            system("cls");
+            checkDetail();
+            break;
+
+        case 5:
+            transaction_details();
+            break;
+
+        case 6:
+            system("cls");
+            LastDetail();
+            exit(0);
+
+        default:
+            printf("*****Invalid choice*****");
+        }
+    }
+
+    return 0;
+}
