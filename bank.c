c70553552787ace0c732ed52874820573603daae
Mahi Maliha <87617184+MahiMaliha@users.noreply.github.com>
2022-09-16 19:52:22 +0600

    added all variables

--- bank.c
+++ bank.c
@@ -4,3 +4,15 @@
 #include <stdlib.h> //exit(0),system()
 #include <conio.h>  //getch()
 #include <time.h>   //time(),ctime()
+char name[20];
+int dip_amt, amt = 10000, acc_no, ac, count = 0; // Global variables
+int trans_amt;
+int with_amt;
+
+void deposit_money();
+void withdraw_money();
+void transfer_money();
+void checkDetail();
+void LastDetail();
+void transaction_details();
+void menu();
